1438ab7c1aab2556ffeb275e1bcd3358cdf0bb03
Yue Hu <sammo3182@sina.com>
2021-09-15 12:51:13 +0800

    descriptive data done; fixed #2

--- R/compare_supdem_cri.R
+++ R/compare_supdem_cri.R
@@ -1,42 +1,60 @@
 library(tidyverse)
 library(DCPOtools)
+library(here)
+
+# Data cleaning and checking ---------------------------------------------------------
 
 sd <- read_csv(here::here("data", "supdem raw survey marginals.tab"), col_types = "cdcddcdc") %>% 
     mutate(item_fam = str_extract(tolower(Item), "^[a-z]+"),
            country = countrycode::countrycode(Country, "country.name", "country.name"),
            dataset = "supdem") %>% 
     rename(year = Year, project = Project) %>% 
-    with_min_yrs(2)
-
-# claassen_input_raw <- DCPOtools:::claassen_setup(vars = read_csv(here::here("data-raw", "mood_dem.csv"),
-#                                                                  col_types = "cccccc"),
-#                                                  file = "data/claassen_input_raw.csv")
+    with_min_yrs(2) # Selecting data w. at least two years
 
 claassen_input_raw <- read_csv(here::here("data", "claassen_input_raw.csv"),
                                col_types = "cdcddcd")
 
-claassen_input_raw1 <- claassen_input_raw %>% 
-    filter(!((str_detect(item, "army_wvs") & # WVS obs identified as problematic by Claassen 
-                  ((country=="Albania" & year==1998) |
-                       (country=="Indonesia" & (year==2001 | year==2006)) |
-                       (country=="Iran" & year==2000) |
-                       (country=="Pakistan" & (year==1997 | year==2001)) | # 1996 in Claassen
-                       (country=="Vietnam" & year==2001))) |
-                 (str_detect(item, "strong_wvs") &
-                      ((country=="Egypt" & year==2012) |
-                           (country=="Iran" & (year==2000 | year==2007)) | # 2005 in Claassen
-                           (country=="India") |
-                           (country=="Pakistan" & (year==1997 | year==2001)) | # 1996 in Claassen
-                           (country=="Kyrgyzstan" & (year==2003 | year==2011)) |
-                           (country=="Romania" & (year==1998 | year==2005 | year==2012)) |
-                           (country=="Vietnam" & year==2001))) |
-                 (country %in% c("Puerto Rico", "Northern Ireland", 
-                                 "SrpSka Republic", "Hong Kong SAR China")))) %>%
+claassen_input_raw <- claassen_input_raw %>%
+    filter(!((
+        str_detect(item, "army_wvs") &
+            # WVS obs identified as problematic by Claassen
+            ((country == "Albania" & year == 1998) |
+                 (country == "Indonesia" &
+                      (year == 2001 | year == 2006)) |
+                 (country == "Iran" & year == 2000) |
+                 (country == "Pakistan" &
+                      (year == 1997 | year == 2001)) | # 1996 in Claassen
+                 (country == "Vietnam" & year == 2001)
+            )
+    ) |
+        (
+            str_detect(item, "strong_wvs") &
+                ((country == "Egypt" & year == 2012) |
+                     (country == "Iran" &
+                          (year == 2000 | year == 2007)) | # 2005 in Claassen
+                     (country == "India") |
+                     (country == "Pakistan" &
+                          (year == 1997 | year == 2001)) | # 1996 in Claassen
+                     (country == "Kyrgyzstan" &
+                          (year == 2003 | year == 2011)) |
+                     (country == "Romania" &
+                          (year == 1998 | year == 2005 | year == 2012)) |
+                     (country == "Vietnam" & year == 2001)
+                )
+        ) |
+        (
+            country %in% c(
+                "Puerto Rico",
+                "Northern Ireland",
+                "SrpSka Republic",
+                "Hong Kong SAR China"
+            )
+        ))) %>%
     with_min_yrs(2)
 
 claassen_input <- DCPOtools:::format_claassen(claassen_input_raw)
 
-cri <- claassen_replication_input$data %>% 
+cri <- claassen_input$data %>% 
     mutate(p_dcpo = str_extract(survey, "^[a-z]+"), 
            project = case_when(p_dcpo == "afrob" ~ "afb",
                                p_dcpo == "amb" ~ "lapop",
@@ -62,31 +80,31 @@ no_problems <- inner_join(sd %>% select(-dataset),
            diff_samp = round(Sample - samp)) %>% 
     select(-CAbb, -COWCode)
 
-ggplot(no_problems, aes(x = prop_ours, y = prop_cls, color = p_dcpo)) + 
-    geom_point(shape = 19,
-               alpha = .2) + 
-    theme_bw() +
-    labs(y = "Proportion Supporting Democracy in Claassen's Replication File",
-         x = "Proportion Supporting Democracy in Currently Available Survey Data") +
-    annotate("text", x = .22, y = .95, label = "evdemoc_asiab accounts\nfor most overreports") +
-    annotate("text", x = .75, y = .23, label = "impdemoc_pew accounts for\nmost large underreports") +
-    scale_color_discrete(name="Survey\nProgram")
+saveRDS(no_problems, here("data", "data_comparison.rds"))
 
-ggsave('paper/ourdata_vs_claassensdata.pdf', width = 7)
+# Correlation -------------------------------------------------------------------------
 
-needed <- anti_join(sd, cri, by = c("country", "year", "item_fam", "project")) # 311 obs 
+needed <- anti_join(sd, cri, by = c("country", "year", "item_fam", "project")) 
+# 309 obs Old comment: 311 obs 
 # note: I have confirmed that Dominican Rep lb 2003; Spain lb 2015; and Bahrain wvs 2014 do not exist,
 # which account for 6 (and so drop Bahrain 2006, accounting for 4 more)
 
-available <- anti_join(cri, sd, by = c("country", "year", "item_fam", "project")) #1420 obs
+available <- anti_join(cri, sd, by = c("country", "year", "item_fam", "project")) 
+# 1310 obs; Old comment: 1420 obs
 
 corrs <- no_problems %>%
     group_by(country) %>% 
-    summarize(corr = cor(Response/Sample, x/samp))
+    summarize(corr = cor(Response/Sample, x/samp)) %>% 
+    arrange(corr)
 
+saveRDS(corrs, here("data", "data_correlation.rds"))
 
 ## Does Claassen use the exact same data in both articles?  Yep
-compare_supdem <- read.csv("~/Sources/FSLibrary/Claassen2020/dem_mood_apsr.csv") %>%
+
+# load(here("data", "dem_mood_apsr.rda"))
+# saveRDS(dem_mood_apsr, here("data", "dem_mood_apsr.rds")) # using rds for later reading as an object
+
+compare_supdem <- readRDS(here("data", "dem_mood_apsr.rds")) %>%
     select(Country, Year, apsr_supdem_trim = SupDem_trim) %>%
     full_join(read.csv(here::here("data", "Support_democracy_ajps.csv")) %>%
                   select(Country, Year, ajps_supdem_trim = SupDem_trim)) %>%
---
+++ R/customizedFunctions.R
@@ -0,0 +1,91 @@
+parse_na_types <- function(s) {
+    
+    positions <- purrr::map(
+        stringr::str_split(s, pattern = ""), 
+        match,
+        table = names(na_types_dict)
+    ) %>%
+        unlist()
+    
+    na_types_dict[positions] %>%
+        unlist() %>%
+        unname()
+}
+
+na_types_dict <- list("r" = NA_real_,
+                      "i" = rlang::na_int,
+                      "c" = NA_character_,
+                      "l" = rlang::na_lgl)
+
+as_glance_tibble <- function(..., na_types) {
+    
+    cols <- list(...)
+    
+    if (length(cols) != stringr::str_length(na_types)) {
+        stop(
+            "The number of columns provided does not match the number of ",
+            "column types provided."
+        )
+    }
+    
+    na_types_long <- parse_na_types(na_types)
+    
+    entries <- purrr::map2(cols, 
+                           na_types_long, 
+                           function(.x, .y) {if (length(.x) == 0) .y else .x})
+    
+    tibble::as_tibble_row(entries)
+    
+}
+
+tidy.pgmm <- function(x,
+                      conf.int = FALSE,
+                      conf.level = 0.95,
+                      ...) {
+    result <- summary(x)$coefficients %>%
+        tibble::as_tibble(rownames = "term") %>%
+        dplyr::rename(
+            estimate = Estimate,
+            std.error = `Std. Error`,
+            statistic = `z-value`,
+            p.value = `Pr(>|z|)`
+        )
+    
+    if (conf.int) {
+        ci <- confint(x, level = conf.level) %>% 
+            as.data.frame() %>% 
+            rownames_to_column(var = "term") %>% 
+            dplyr::rename(
+                conf.low = `2.5 %`,
+                conf.high = `97.5 %`
+            )
+        result <- dplyr::left_join(result, ci, by = "term")
+    }
+    
+    result
+}
+
+
+glance.pgmm <- function(x, ...) {
+    s <- summary(x)
+    as_glance_tibble(
+        nobs = stats::nobs(x),
+        n.country = pdim(x)$nT$n,
+        na_types = "ii"
+    )
+}
+
+glance.plm <- function(x, ...) {
+    s <- summary(x)
+    as_glance_tibble(
+        nobs = stats::nobs(x),
+        n.country = pdim(x)$nT$n,
+        na_types = "ii"
+    )
+}
+
+vcovBK_se <- function(x) {
+    plm::vcovBK(x, cluster = "time") %>% 
+        diag() %>% 
+        sqrt()
+}
